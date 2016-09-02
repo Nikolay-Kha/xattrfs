cdaae2e56b6420a3b04adef503d64c58cbc9a6d3
Nikolay Khabarov <2xl@mail.ru>
2016-09-02 19:50:37 +0300

    handling blob data properly in sqlite

--- xattrdb.c
+++ xattrdb.c
@@ -6,7 +6,7 @@
 
 static sqlite3 *db = NULL;
 
-#define ENSURE_TABLE_EXISTS "CREATE TABLE IF NOT EXISTS '%s'(name unique, value);"
+#define SQL_REQUEST_MAX_SIZE 16768
 
 typedef struct {
     char *value;
@@ -24,45 +24,59 @@ bool xattrdb_close() {
     return res == SQLITE_OK;
 }
 
-bool xattrdb_set(const char *path, const char *name, const char *value, unsigned int len) {
-    char req[32768];
-    snprintf(req, sizeof(req),
-        ENSURE_TABLE_EXISTS
-        "INSERT OR REPLACE INTO '%s'(name, value) VALUES ('%s', '%.*s');",
-        path, path, name, len, value);
-    const int res = sqlite3_exec(db, req, NULL, NULL, NULL);
-    return res == SQLITE_OK;
+static void ensure_table_exists(const char *path) {
+    char req[SQL_REQUEST_MAX_SIZE];
+    snprintf(req, sizeof(req), "CREATE TABLE IF NOT EXISTS '%s'(name unique, value);", path);
+    sqlite3_exec(db, req, NULL, NULL, NULL);
 }
 
-static int get_callback(void *data, int argc, char **argv, char **azColName){
-    cb_data *cbdata = (cb_data*)data;
-    if(argc < 1) {
-        return 0;
+bool xattrdb_set(const char *path, const char *name, const char *value, unsigned int len) {
+    ensure_table_exists(path);
+    char req[SQL_REQUEST_MAX_SIZE];
+    int reqsize = snprintf(req, sizeof(req),
+        "INSERT OR REPLACE INTO '%s'(name, value) VALUES ('%s', ?);",
+        path, name);
+    sqlite3_stmt *stmt;
+    int res = sqlite3_prepare_v2(db, req, reqsize + 1, &stmt, NULL);
+    if(res == SQLITE_OK) {
+        res = sqlite3_bind_blob(stmt, 1, value, len, SQLITE_STATIC);
+        if(res == SQLITE_OK) {
+            res = sqlite3_step(stmt);
+        }
     }
-    if(cbdata->value) {
-        *cbdata->num = snprintf(cbdata->value, cbdata->len, "%s", argv[0]);
-    } else {
-        *cbdata->num = strlen(argv[0]) + 1;
-    }
-    return 0;
+    sqlite3_finalize(stmt);
+    return res == SQLITE_DONE;
 }
 
 bool xattrdb_get(const char *path, const char *name, unsigned int *out_num, char *out_value, unsigned int out_len) {
-    char req[32768];
-    snprintf(req, sizeof(req),
-        ENSURE_TABLE_EXISTS
+    ensure_table_exists(path);
+    char req[SQL_REQUEST_MAX_SIZE];
+    int reqsize = snprintf(req, sizeof(req),
         "SELECT value FROM '%s' WHERE name = '%s';",
-        path, path, name);
-    cb_data data;
-    data.value = out_value;
-    data.len = out_len;
-    data.num = out_num;
-    *data.num = 0;
-    if(data.len == 0) {
-        data.value = NULL;
+        path, name);
+    *out_num = 0;
+    sqlite3_stmt *stmt;
+    int res = sqlite3_prepare_v2(db, req, reqsize + 1, &stmt, NULL);
+    if(res == SQLITE_OK) {
+        res = sqlite3_step(stmt);
+        if (res == SQLITE_ROW) {
+            int len = sqlite3_column_bytes(stmt, 0);
+            if(out_value != NULL && out_len != 0) {
+                if(len > out_len) {
+                    len = out_len;
+                }
+                char *data = (char *)sqlite3_column_blob(stmt, 0);
+                memcpy(out_value, data, len);
+            }
+            *out_num = len;
+            res = SQLITE_DONE;
+        } else if(res == SQLITE_DONE) {
+            *out_num = 0;
+        }
     }
-    const int res = sqlite3_exec(db, req, get_callback, (void *)&data, NULL);
-    return res == SQLITE_OK;
+    sqlite3_finalize(stmt);
+
+    return res == SQLITE_DONE;
 }
 
 static int list_callback(void *data, int argc, char **argv, char **azColName){
@@ -89,11 +103,11 @@ static int list_callback(void *data, int argc, char **argv, char **azColName){
 }
 
 bool xattrdb_list(const char *path, unsigned int *out_num, char *out_buf, unsigned int out_len) {
-    char req[32768];
+    ensure_table_exists(path);
+    char req[SQL_REQUEST_MAX_SIZE];
     snprintf(req, sizeof(req),
-        ENSURE_TABLE_EXISTS
         "SELECT name FROM '%s';",
-        path, path);
+        path);
     cb_data data;
     data.value = out_buf;
     data.len = out_len;
@@ -107,7 +121,7 @@ bool xattrdb_list(const char *path, unsigned int *out_num, char *out_buf, unsign
 }
 
 bool xattrdb_removename(const char *path, const char *name) {
-    char req[32768];
+    char req[SQL_REQUEST_MAX_SIZE];
     snprintf(req, sizeof(req),
         "DELETE FROM '%s' WHERE name = '%s';",
         path, name);
@@ -116,7 +130,7 @@ bool xattrdb_removename(const char *path, const char *name) {
 }
 
 bool xattrdb_removepath(const char *path) {
-    char req[32768];
+    char req[SQL_REQUEST_MAX_SIZE];
     snprintf(req, sizeof(req),
         "DROP TABLE '%s';",
         path);
@@ -125,7 +139,7 @@ bool xattrdb_removepath(const char *path) {
 }
 
 bool xattrdb_renamepath(const char *oldpath, const char *newpath) {
-    char req[32768];
+    char req[SQL_REQUEST_MAX_SIZE];
     snprintf(req, sizeof(req),
         "ALTER TABLE '%s' RENAME TO '%s';",
         oldpath, newpath);
